021fba02aaa93d8c7b1847bd0105e558d6b60517
rdx <31172722+rd-xx@users.noreply.github.com>
2022-12-15 15:14:22 +0100

    fix media going to the wrong dir

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
 		price = models.FloatField(default=0.0)
 		stock = models.IntegerField(default=0)
 		description = models.TextField(blank=True)
-		image = models.ImageField(upload_to='media/products')
+		image = models.ImageField(upload_to='products')
 
 		def __str__(self):
 				return f"{self.name} - {self.stock}"
